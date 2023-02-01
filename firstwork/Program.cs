9215b6c1cde1e540005c4dd01d45609cc0265575
divinejoshua <divineuche399@gmail.com>
2023-02-01 20:47:43 +0000

    Added profile

--- firstwork/Program.cs
+++ firstwork/Program.cs
@@ -1,7 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World again!");
+builder.Services.AddRazorPages();
+
+//for static files 
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapRazorPages();
+
+//app.MapGet("/", () => "Hello World again!");
+
 
 app.Run(); 
 
